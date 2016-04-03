1f750bc3657f6bda656a094adaf8275466412bc9
Alfi <alfirahadian@gmail.com>
2016-04-03 22:03:07 +0700

    Update Info Rekruitasi

---
+++ application/views/view_home.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+      <title>CNC Home</title>
+      <!-- Compiled and minified CSS -->
+      <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/custom.css">
+      <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/materialize.css">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+      <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+      <!-- Compiled and minified JavaScript -->
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      
+      <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+      <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+      <!-- For load material icon -->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <script src="<?php echo base_url();?>assets/js/materialize.js"></script>
+      <script type="text/javascript">
+    $(document).ready(function() {
+    $('select').material_select();
+  });
+  </script>
+</head>
+<body>
+<main>
+<div class="section no-pad-bot" id="index-banner">
+        <div class="container">
+        <center><img class="header materialboxed" width="200" src="<?php echo base_url();?>assets/img/cnc.png"></center>
+          <h4 class="header center">Computer and Communication Laboratory</h4>
+          <div class='row center'>
+            <h5 class ="header col s12 light center">O Building Room 203, School of Electrical Engineering</h5>
+            <br>
+            <h5 class ="header col s12 light center">Telkom University</h5>
+          </div>
+          </div>
+        <br><br>
+</div>
+<div class="section blue-grey lighten-4">
+    <h5 class="header col s12 light center"><span><i class="fa fa-info-circle"></i><b>     INFO REKRUITASI</b></span></h5>
+</div>
+<div class="section">
+  <div class="row">
+     <div class="col s12 m4 l4 ">
+                <div class="card-panel center z-depth-3">
+                  <p><b><h6>Rekruitasi<br><br>RESEARCH ASSISTANT<br><br>2016-2017</h6></b><br>
+                  </p>
+                  <a class="waves-effect waves-light btn-large light-blue darken-4" href="riset">Research Assistant</a>
+                </div>                          
+      </div>
+      <div class="col s12 m4 l4">
+                <div class="card-panel center z-depth-3">
+                  <p><b><h6>Rekruitasi<br><br>LABORATORY ASSISTANT<br><br>2016-2017</h6></b><br>
+                  </p>
+                  <a class="waves-effect waves-light btn-large orange darken-3" href="aslab">Laboratory Assistant</a>
+                </div>                          
+      </div>
+      <div class="col s12 m4 l4">
+                <div class="card-panel center z-depth-3">
+                 <p><b><h6>KEEP CALM</h6></b></p>
+                  <i class="fa fa-question-circle fa-5x"></i>
+                  <p><b><h6>COMING SOON</h6></b></p><br>
+                </div>                          
+      </div>
+  </div>
+</div>
+<div class="section">
+  <div class="row">
+        <div class="container">
+          <h5 class ="header col s12 light center"><i>"Kegagalan adalah awal dari kegagalan lainnya."</i></h5>
+          <i>
+          <h5 class="right">- Hernanta -</h5>
+        </i>
+      </div>
+    </div>
+</div>
+</main>
+<footer class="page-footer">
+        <div class="footer-copyright">
+                Made with <i class="fa fa-heart"></i> <a class="grey-text text-lighten-4" href="http://labcnc.com/">from O203 Room.</a>
+                <span class="right"> Copyright © 2016 <a class="grey-text text-lighten-1" href="http://labcnc.com" target="_blank"></a> All rights reserved.</span>
+        </div>
+</footer>
+</body>
+</html>
